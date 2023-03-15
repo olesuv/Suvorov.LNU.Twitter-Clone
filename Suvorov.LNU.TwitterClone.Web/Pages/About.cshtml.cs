5291c657a70d99611f830e620b788cbcdfe99373
plxgwalker <olegsuv.ukr@gmail.com>
2023-03-15 19:43:41 +0200

    Added About page

---
+++ Suvorov.LNU.TwitterClone.Web/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Suvorov.LNU.TwitterClone.Web.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
